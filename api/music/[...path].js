234ac7e78c6b53794fd482bf92dd6e8b64d3905a
ignite-boy <support@milanlife.in>
2026-09-10 14:23:17 +0000

    fix: route music API through dedicated Vercel handler

--- api/music/[...path].js
+++ api/music/[...path].js
@@ -6,9 +6,14 @@ const musicRouter = require('../../backend/routes/music');
 const app = express();
 
 app.disable('x-powered-by');
+
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: false, limit: '1mb' }));
 
+// Mount the existing Music Express router.
 app.use('/', musicRouter);
 
-module.exports = app;
+// Vercel-compatible handler.
+module.exports = (req, res) => {
+  return app(req, res);
+};
