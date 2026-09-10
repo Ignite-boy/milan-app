57057e09d09cd2c75b2c4b7a3b678bf5d3e0285a
ignite-boy <support@milanlife.in>
2026-09-10 14:19:17 +0000

    fix: correct music serverless router path

--- api/music/[...path].js
+++ api/music/[...path].js
@@ -1,7 +1,14 @@
 'use strict';
 
-const musicRouter = require('../../../backend/routes/music');
+const express = require('express');
+const musicRouter = require('../../backend/routes/music');
 
-module.exports = function handler(req, res) {
-  return musicRouter(req, res);
-};
+const app = express();
+
+app.disable('x-powered-by');
+app.use(express.json({ limit: '1mb' }));
+app.use(express.urlencoded({ extended: false, limit: '1mb' }));
+
+app.use('/', musicRouter);
+
+module.exports = app;
